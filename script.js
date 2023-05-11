bf1a9ab497feabe3f2549075b0f1d3e4e8c5e206
Rohit Routh <49668692+RohitRouth@users.noreply.github.com>
2023-05-11 18:22:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,37 +1,44 @@
 class OutOfRangeError extends Error {
-  constructor() {
-    super('Expression should only consist of integers and +-/* characters');
-    this.name = 'OutOfRangeError';
+  constructor(arg) {
+    super(`Expression should only consist of integers and +-/* characters and not ${arg}`);
+    this.name = "OutOfRangeError";
   }
 }
 
 class InvalidExprError extends Error {
   constructor() {
-    super('Expression should not have an invalid combination of expression');
-    this.name = 'InvalidExprError';
+    super(`Expression should not have an invalid combination of expression`);
+    this.name = "InvalidExprError";
   }
 }
 
 function evalString(expr) {
-  if (expr.match(/[^0-9+\-*/\s()]/g)) {
-    throw new OutOfRangeError();
-  }
+  const operators = ["+", "-", "*", "/"];
 
-  if (expr.match(/[*+\/-][*+\/]/g)) {
+  // Check for invalid expressions
+  if (!/^[\d\s\+\-\*\/]*$/.test(expr)) {
+    throw new OutOfRangeError(expr);
+  }
+  
+  // Check for invalid combinations of operators
+  if (/[+\-*\/]{2}/.test(expr)) {
     throw new InvalidExprError();
   }
-
-  if (expr.match(/^[*+\/]/)) {
-    throw new SyntaxError('Expression should not start with invalid operator');
+  
+  // Check for invalid starting and ending operators
+  if (/^[+\/*]/.test(expr)) {
+    throw new SyntaxError("Expression should not start with invalid operator");
+  }
+  if (/[+\/*-]$/.test(expr)) {
+    throw new SyntaxError("Expression should not end with invalid operator");
   }
 
-  if (expr.match(/[*+\/-]$/)) {
-    throw new SyntaxError('Expression should not end with invalid operator');
-  }
+  return eval(expr);
+}
 
-  try {
-    return eval(expr);
-  } catch (e) {
-    throw new SyntaxError('Expression is not valid');
-  }
+try {
+  const result = evalString("1 + 2 * 3 - 4 / 2");
+  console.log(result); // expected output: 6
+} catch (err) {
+  console.error(err);
 }
