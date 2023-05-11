559cf8d0636e1b72981922f98600ffab6e21821c
Rohit Routh <49668692+RohitRouth@users.noreply.github.com>
2023-05-11 18:28:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,38 +7,56 @@ class OutOfRangeError extends Error {
 
 class InvalidExprError extends Error {
   constructor() {
-    super(`Expression should not have an invalid combination of expression`);
+    super("Expression should not have an invalid combination of expression");
     this.name = "InvalidExprError";
   }
 }
 
-function evalString(expr) {
+function evalString(expression) {
   const operators = ["+", "-", "*", "/"];
+  const startOperator = ["+", "*", "/"];
+  const endOperator = ["+", "-", "*", "/"];
 
-  // Check for invalid expressions
-  if (!/^[\d\s\+\-\*\/]*$/.test(expr)) {
-    throw new OutOfRangeError(expr);
+  // Check for invalid operators
+  for (let i = 0; i < expression.length - 1; i++) {
+    const currentChar = expression.charAt(i);
+    const nextChar = expression.charAt(i + 1);
+    if (operators.includes(currentChar) && operators.includes(nextChar)) {
+      throw new InvalidExprError();
+    }
   }
-  
-  // Check for invalid combinations of operators
-  if (/[+\-*\/]{2}/.test(expr)) {
-    throw new InvalidExprError();
-  }
-  
-  // Check for invalid starting and ending operators
-  if (/^[+\/*]/.test(expr)) {
+
+  // Check for start and end operators
+  if (startOperator.includes(expression.charAt(0))) {
     throw new SyntaxError("Expression should not start with invalid operator");
   }
-  if (/[+\/*-]$/.test(expr)) {
+  if (endOperator.includes(expression.charAt(expression.length - 1))) {
     throw new SyntaxError("Expression should not end with invalid operator");
   }
 
-  return eval(expr);
+  // Check for invalid characters
+  const regex = /^[0-9+\-*/\s]+$/;
+  if (!regex.test(expression)) {
+    throw new OutOfRangeError(expression);
+  }
+
+  return eval(expression);
 }
 
 try {
-  const result = evalString("1 + 2 * 3 - 4 / 2");
-  console.log(result); // expected output: 6
-} catch (err) {
-  console.error(err);
+  console.log(evalString("3 + 4"));
+  console.log(evalString("1-1"));
+  console.log(evalString("2 /2"));
+  console.log(evalString("23+35"));
+  console.log(evalString("3-3"));
+  console.log(evalString("22*3"));
+  console.log(evalString("45 / 5"));
+  console.log(evalString("4 + 4 / 4"));
+  console.log(evalString("4 +"));
+  console.log(evalString("+3"));
+  console.log(evalString("3 + +4"));
+  console.log(evalString("3++4"));
+  console.log(evalString("4+4-"));
+} catch (error) {
+  console.log(error.message);
 }
